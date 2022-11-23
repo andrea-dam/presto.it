252e58cd0af151159d15b22748154bbc48e28696
MariaVittoria Imbimbo <mariavittoria.imbimbo@gmail.com>
2022-11-23 12:43:35 +0100

    aggiunto titolo

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,7 +1,7 @@
 
 <nav class="navbar shadow navbar-expand-lg navbar-dark">
     <div class="container-fluid">
-        <a class="navbar-brand" href="{{route('homepage')}}">Navbar</a>
+        <a id="title" class=" navbar-brand" href="{{route('homepage')}}">Presto.it</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -1,5 +1,8 @@
 <x-layout>
-    <div class="sfondo">
+    <div class="sfondo text-center container-fluid align-items-center d-flex justify-content-center">
+        <div class="row">
+            <h1 id="title" class="col display-1 text-dark">presto.it</h1>
+        </div>
     </div>
 
 </x-layout>
