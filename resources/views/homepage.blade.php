2578700267e4f53c6f7c7dbd7a02744b37414856
Andrea Damiani <andrea.dam90@gmail.com>
2022-11-26 00:00:09 +0100

    Resa la homepage ancora più responsive

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -16,7 +16,7 @@
     {{-- Sezione Categorie --}}
     <section class="container text-center my-5">
         <h2 id="tutte-le-categorie" class="">Tutte le Categorie</h2>
-        <div class="row row-cols-1 row-cols-md-3 row-cols-lg-5 justify-content-center">
+        <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-5 justify-content-center">
             <div class="col distanza-categorie">
                 <a class="text-decoration-none" href="{{route('category.index', ['id' => 1])}}">
                     <div class="rounded sfondo-categorie p-3 pt-0 position-relative h-100 d-flex flex-column justify-content-around">
