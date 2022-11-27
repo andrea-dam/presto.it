ebf0e36b2218dcd6ff5bc6c626691c29d442d630
Andrea Damiani <andrea.dam90@gmail.com>
2022-11-27 22:02:54 +0100

    Ulteriormente migliorato frontend pagina login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,5 +1,5 @@
 <x-layout>
-
+    
     <main class="container my-5">
         <div class="row justify-content-center">  
             {{-- Se uno o più dati non sono stati inseriti correttamente --}}
@@ -22,8 +22,9 @@
                 <form class="form-user  p-5 rounded" method="POST" action="{{route('login')}}">
                     @csrf
                     {{-- <h2>Accedi:</h2> --}}
-                    <div class="position-relative">
-                        <a href="{{route('homepage')}}" role="button" class="btn position-absolute top-50 end-0 translate-middle-y p-3 text-light"><i class="fa-solid fa-house"></i></a>
+                    <div class="d-flex justify-content-between align-items-center">
+                        <h3>Accedi</h3>
+                        <a href="{{route('homepage')}}" role="button" class="btn text-light"><i class="fa-solid fa-house"></i></a>
                     </div>
                     <div class="mb-4 mt-4">
                         <label for="email" class="form-label">Indirizzo Email</label>
@@ -31,9 +32,13 @@
                     </div>
                     <div class="mb-4">
                         <label for="password" class="form-label">Password</label>
-                        <div class="position-relative">
-                            <button class="eye position-absolute top-50 end-0 translate-middle-y" type="button" onclick="showPwd()" ><i class="fa-solid fa-eye"></i></button>
+                        <div class="input-group">
                             <input type="password" class="form-control" id="password" name="password">
+                            <span class="input-group-text">
+                                <i id="icona" class="fa-solid fa-eye eye" role="button" onclick="showPwd()">
+                                    {{-- <button class="" type="button" onclick="showPwd()"></button> --}}
+                                </i>
+                            </span>
                         </div>
                     </div>
                     <div class="mb-4 form-check d-flex justify-content-between">
@@ -43,21 +48,29 @@
                         </div>
                         <a href="{{route('register')}}" class="text-decoration-none text-light">Non hai un account? Registrati</a>
                     </div>
-                    <button type="submit" class="btn3 border-light btn text-light me-3 ">Accedi</button>
-
+                    <button type="submit" class="btn3 btn border me-3">Accedi</button>
                 </form>
             </div>
         </div>
     </main>
-
+    
     <script>
         function showPwd() {
-          var input = document.getElementById('password');
-          if (input.type === "password") {
-            input.type = "text";
-          } else {
-            input.type = "password";
-          }
+            let isClicked = false;
+            // Campo di input
+            var input = document.getElementById('password');
+            // Icona
+            let icon = document.querySelector('#icona');
+            
+            if (input.type === "password") {
+                input.type = "text";
+                icon.classList.remove('fa-eye');
+                icon.classList.add('fa-eye-slash');
+            } else {
+                input.type = "password";
+                icon.classList.remove('fa-eye-slash');
+                icon.classList.add('fa-eye');
+            }
         }
-  </script>
+    </script>
 </x-layout>
