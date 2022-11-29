a4b27ef15c1edc888308a27ab64e0a53f97bfb39
Andrea Damiani <andrea.dam90@gmail.com>
2022-11-29 21:26:33 +0100

    Compilato file /en/ui.php

--- lang/it/ui.php
+++ lang/it/ui.php
@@ -1,10 +1,10 @@
 <?php
 
-return[
+return [
     // Navbar
     'home' => 'Home',
     'newAnnouncement' => 'Inserisci annuncio',
-    'auditorArea' => 'Area Revisore',
+    'revisorArea' => 'Area Revisore',
     'allAnnouncement' => 'Tutti gli Annunci',
     'welcome' => 'Benvenutə',
     'login' => 'Accedi',
@@ -12,22 +12,22 @@ return[
     'logout' => 'Esci',
     'search' => 'Cerca fra gli annunci',
 
-    // Inserisci un annuncio
-    'enterAnnouncement' => 'Inserisci un Annuncio',
+    // // Inserisci un annuncio
+    // 'enterAnnouncement' => 'Inserisci un Annuncio',
 
     // Tutti gli Annunci
         // 'allAnnouncement' => 'Tutti gli Annunci',
-    'noFound' => 'Non ci sono annunci per questa ricerca.',
+    'notFound' => 'Non ci sono annunci per questa ricerca.',
 
     // Scopri di più
     'fastShipping' => 'Spedizione rapida',
-    'evidence' => 'Prova Plus gratis per 30 giorni',
-    'expedition' => 'Spedizione standard',
-    'gratis' => 'Gratis',
-    'reviews' => 'Recensioni (0)',
+    'freeTrial' => 'Prova Plus gratis per 30 giorni',
+    'shipping' => 'Spedizione standard',
+    'free' => 'Gratis',
+    'reviews' => 'Recensioni',
     'packaging' => 'Imballaggio sostenibile',
-    'expeditions' => 'Tutte le confezioni usate per le spedizioni sono in carta 100% riciclata e reciclabile, prive di plastica.',
-    'rendered' => 'Resi facili',
+    'shippings' => 'Tutte le confezioni usate per le spedizioni sono in carta 100% riciclata e reciclabile, prive di plastica.',
+    'simpleReturns' => 'Resi facili',
     'goods' => 'Hai 14 giorni lavorativi dalla data di ricevimento della merce per esercitare diritto al recesso.',
     'securePayments' => 'Pagamenti sicuri',
     'payments' => 'Pagamenti con carta di credito, Paypal, Bonifico bancario diretto, Scalapay e contrassegno.',
@@ -51,10 +51,10 @@ return[
         // 'backToHome' => 'Torna alla Home',
 
     // Email revisore
-    'requestToCollaborate' => 'ha richiesto di collaborare con noi.',
-    'name' => 'Nome',
-    'email' => 'Email',
-    'makeAuditor' => 'Rendi revisore',
+    // 'requestToCollaborate' => 'ha richiesto di collaborare con noi.',
+    // 'name' => 'Nome',
+    // 'email' => 'Email',
+    // 'makeRevisor' => 'Rendi revisore',
 
     // Pagina revisore
     'rejectAd' => 'Rifiuta l\'annuncio',
@@ -97,7 +97,7 @@ return[
     'contacts' => 'Contatti',
     'warning' => 'Avviso',
     'message' => 'Cliccando sul pulsante Continua verrà inviata una mail all\'admin di Presto.it con i tuoi dati per verificare la tua richiesta.',
-    'continues' => 'Continua',
-    'annuls' => 'Annulla',
-    'becomeReviewer' => 'Diventa Revisore',
+    'continue' => 'Continua',
+    'cancel' => 'Annulla',
+    'becomeRevisor' => 'Diventa Revisore',
 ];
