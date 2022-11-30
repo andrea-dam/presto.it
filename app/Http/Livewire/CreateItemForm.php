2f10ece05094a2066ea9988d59f7afb040e9994a
Andrea Damiani <andrea.dam90@gmail.com>
2022-11-30 22:20:01 +0000

    Revert "Merge branch 'master' into 'main'"
    This reverts merge request !1

--- app/Http/Livewire/CreateItemForm.php
+++ app/Http/Livewire/CreateItemForm.php
@@ -5,7 +5,6 @@ namespace App\Http\Livewire;
 use App\Models\Item;
 use Livewire\Component;
 use App\Models\Category;
-use App\Jobs\RemoveFaces;
 use App\Jobs\ResizeImage;
 use Livewire\WithFileUploads;
 use App\Jobs\GoogleVisionLabelImage;
@@ -64,17 +63,15 @@ class CreateItemForm extends Component
         
         if(count($this->images)){
             foreach($this->images as $image){
-                
                 $newFileName = "items/{$item->id}";
                 
                 $newImage = $item->images()->create(['path'=>$image->store($newFileName, 'public')]);
        
-                RemoveFaces::withChain([
-                    new ResizeImage($newImage->path, 1200, 900),
-                    new ResizeImage($newImage->path, 400, 300),
-                    new GoogleVisionSafeSearch($newImage->id),
-                    new GoogleVisionLabelImage($newImage->id)
-                ])->dispatch($newImage->id);
+                dispatch(new ResizeImage($newImage->path, 1200, 900));
+                dispatch(new ResizeImage($newImage->path, 400, 300));
+                dispatch(new GoogleVisionSafeSearch($newImage->id));
+                dispatch(new GoogleVisionLabelImage($newImage->id));
+
             }
             File::deleteDirectory(storage_path('/app/livewire-tmp'));
         }
